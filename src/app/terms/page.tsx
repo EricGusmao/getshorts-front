5eeea7e89ccdbda307d1ec9df865fcef44257717
slocksert <rsdlj01@gmail.com>
2024-11-29 21:48:11 -0300

    Add Terms page component to display terms and conditions

---
+++ src/app/terms/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import Footer from "@/components/Footer";
+import Head from "@/components/Head";
+import { Terms } from "@/components/Terms";
+
+export default function Home() {
+  return (
+    <div>
+      <Head />
+      <Terms />
+      <Footer />
+    </div>
+  );
+}
