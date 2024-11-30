714dbbe74e29684e1bffdce34657f98751ee6a22
Eric Gusmao <ericdegusmaopino@gmail.com>
2024-11-30 14:38:20 -0300

    Refactor download button

--- src/app/_components/DownloadButton.tsx
+++ src/app/_components/DownloadButton.tsx
@@ -9,20 +9,11 @@ const DownloadButton: React.FC<DownloadButtonProps> = ({
   videoUrl,
   videoName,
 }) => (
-  <div className="buttonDownload self-center">
-    <Button
-      variant="default"
-      onClick={(e) => {
-        e.preventDefault();
-        const link = document.createElement("a");
-        link.href = videoUrl;
-        link.download = videoName || "video.mp4";
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-      }}
-    >
-      <Download /> Download
+  <div className="self-center">
+    <Button asChild>
+      <a href={videoUrl} download={videoName ?? "video"}>
+        <Download /> Download
+      </a>
     </Button>
   </div>
 );
