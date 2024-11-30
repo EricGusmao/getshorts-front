d073453cf5780f770de61f1745d198ea3f71f798
slocksert <rsdlj01@gmail.com>
2024-11-30 15:06:01 -0300

    Add Slogan component to display service tagline and description

---
+++ src/app/_components/slogan.tsx
@@ -0,0 +1,10 @@
+const Slogan = () => {
+  return (
+    <div className="m-10 flex flex-col justify-center gap-5 text-center text-white">
+      <h1 className="text-2xl font-semibold">Download Service For Shorts</h1>
+      <p className="text-sm italic">Easy, Fast and without Watermark.</p>
+    </div>
+  );
+};
+
+export default Slogan;
