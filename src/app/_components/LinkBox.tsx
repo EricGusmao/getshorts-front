7d3ac35f9534506132c33ddc8d4bda44a9cc92b7
slocksert <rsdlj01@gmail.com>
2024-11-30 12:37:54 -0300

    Refactor LinkBox component to integrate Form, VideoPlayer, VideoDescription, and DownloadButton components

--- src/app/_components/LinkBox.tsx
+++ src/app/_components/LinkBox.tsx
@@ -1,18 +1,11 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useToast } from "@/hooks/use-toast";
 import { Spinner } from "@/components/Spinner";
-import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
-
-import { IoMdDownload } from "react-icons/io";
 import { fetchVideo } from "server/fetchVideo";
+import { Form } from "@/components/Form";
+import DownloadButton from "@/components/DownloadButton";
+import VideoDescription from "@/components/VideoDescription";
+import VideoPlayer from "@/components/VideoPlayer";
 
 const LinkBox = () => {
   const [url, setUrl] = useState<string>("");
@@ -105,88 +98,33 @@ const LinkBox = () => {
     setUrl(event.target.value);
   };
 
+  useEffect(() => {
+    if (videoUrl) {
+      const downloadButton = document.querySelector('.buttonDownload');
+      if (downloadButton) {
+        downloadButton.scrollIntoView({ behavior: 'smooth' });
+      }
+    }
+  }, [videoUrl]);
+
   return (
     <>
-      <div className="">
-        <form
-          onSubmit={SendLink}
-          className="flex flex-row flex-wrap justify-center gap-4"
-        >
-          <Input
-            type="text"
-            className="w-96 text-white"
-            value={url}
-            onChange={handleChange}
-          ></Input>
+      <Form 
+        SendLink={SendLink} handleChange={handleChange} 
+        setDownloadMode={setDownloadMode} setResolution={setResolution} 
+        setUrl={setUrl} url={url}>
+      </Form>
 
-          <Select onValueChange={setResolution} defaultValue="best">
-            <SelectTrigger className="w-28 text-white">
-              <SelectValue placeholder="Quality" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value="144">144p</SelectItem>
-              <SelectItem value="240">240p</SelectItem>
-              <SelectItem value="360">360p</SelectItem>
-              <SelectItem value="480">480p</SelectItem>
-              <SelectItem value="720">720p</SelectItem>
-              <SelectItem value="1080">1080p</SelectItem>
-              <SelectItem value="1440">1440p</SelectItem>
-              <SelectItem value="2160">2160p</SelectItem>
-              <SelectItem value="best">BEST</SelectItem>
-            </SelectContent>
-          </Select>
-
-          <Select onValueChange={setDownloadMode} defaultValue="video">
-            <SelectTrigger className="w-28 text-white">
-              <SelectValue placeholder="Mode" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value="video">Video</SelectItem>
-              <SelectItem value="audio">Audio</SelectItem>
-            </SelectContent>
-          </Select>
-
-          <Button type="submit" variant="outline">
-            SEND
-          </Button>
-        </form>
         {loading && <Spinner size="large" className="mt-16" show={true} />}
-      </div>
 
       {!loading && videoUrl && (
-        <div className="m-5 flex items-center justify-center">
-          <div className="flex max-w-md flex-col rounded-lg bg-zinc-800 p-4 text-white">
-            <div className="mb-4">
-              <video src={videoUrl} className="h-96 w-96" controls />
-            </div>
-            <div className="mb-4 w-72 self-center text-justify">
-              <p>
-                <strong>Channel:</strong> {channelName}
-              </p>
-              <br></br>
-              <p>
-                <strong>Description:</strong> {videoName}
-              </p>
-            </div>
-            <div className="downloadButton self-center">
-              <Button
-                variant="default"
-                onClick={(e) => {
-                  e.preventDefault();
-                  const link = document.createElement("a");
-                  link.href = videoUrl;
-                  link.download = videoName || "video.mp4";
-                  link.className = "buttonDownload";
-                  document.body.appendChild(link);
-                  link.click();
-                  document.body.removeChild(link);
-                }}
-              >
-                <IoMdDownload /> Download
-              </Button>
-            </div>
-          </div>
+        <div className="m-5 flex justify-center items-center">
+        <div className="text-white bg-zinc-800 p-4 rounded-lg max-w-md flex flex-col">
+          <VideoPlayer videoUrl={videoUrl} />
+          <VideoDescription channelName={channelName || ''} videoName={videoName || ''} />
+          <DownloadButton videoUrl={videoUrl} videoName={videoName || ''} />
         </div>
+      </div>
       )}
     </>
   );
