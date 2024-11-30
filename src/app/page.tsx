5dabaaeafd6c38ca1885c28539948ad1c2fc3467
slocksert <rsdlj01@gmail.com>
2024-11-29 21:47:33 -0300

    Add HowToUse and Footer components to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,17 @@
 import Head from "@/components/Head";
 import Slogan from "@/components/Slogan";
 import LinkBox from '@/components/LinkBox';
+import HowToUse from '@/components/HowToUse';
+import Footer from './_components/Footer';
 
 export default function Home() {
   return (
     <div>
-      <Head platform="Twitter"></Head>
-      <Slogan/>
+      <Head />
+      <Slogan />
       <LinkBox/>
+      <HowToUse />
+      <Footer />
     </div>
   );
 }
